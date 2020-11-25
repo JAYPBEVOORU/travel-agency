1ee6a73a7cc78c81d4aac704fd85c94a8367192b
Jayachandran Puttalingaiah <jayachandran.puttalingaiah@reachplc.com>
2020-11-25 15:06:58 +0000

    added knex library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const knex = require('knex');
+
+const db = knex({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test'
+    }
+  });
+
+
 
 const app = new express();
+
+app.get("/health", async (request, response) => {
+    try {
+      const result = await db.select(db.raw('select curdate()'));
+      if (!result) {
+        return res.status(404).send("DB is Unhealthy!");
+      }
+      res.send('Healthy');
+    } catch (e) {
+      console.error(e);
+      res.status(500).send("DB is Unhealthy!");
+    }
+  });
+
 
 app.get('/places',(request, response)=>{
     response.send(['Bangalore','Mysore']);
